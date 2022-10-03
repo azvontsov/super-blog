14e1fecc01ff6e986c76811f3f4c4f8c3ddb92c6
anton <azvontsov@gmail.com>
2022-10-03 15:24:19 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<template lang="">
+    <div class="container">
+        <form>
+            <h4>Create Post</h4>
+            <input class="form-control mt-2" type="text" placeholder="name">
+            <input class="form-control mt-2" type="text" placeholder="description">
+            <div class="d-flex flex-row-reverse bd-highlight">
+                <button class="btn btn-primary mt-2">Create</button>
+              </div>
+        </form>
+        <div class="card mt-3" v-for="post in posts" >
+            <div class="card-body">
+              <h6 class="card-subtitle text-muted">Name:</h6>
+              <h5 class="card-title mb-2">{{ post.title }}</h5>
+              <h6 class="card-subtitle text-muted">Description:</h6>
+              <h5 class="card-title mb-2">{{ post.body }}</h5>
+              <p class="card-text">Some quick example text .</p>
+             
+            </div>
+          </div>
+        
+    </div>
+   
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Description'},
+                {id: 2, title: 'JavaScript', body: 'Description #2'},
+                {id: 3, title: 'JavaScript', body: 'Description #3'},
+                {id: 4, title: 'JavaScript', body: 'Description #4'},
+            ]
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes += 1
+        },
+        addDislike() {
+            this.dislikes += 1
+        }
+    }
 }
+</script>
+<style lang="css">
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .container {
+        margin-top: 20px;
+    }
+    .post {
+        padding: 15px;
+        border: 2px solid teal;
+        margin-top: 15px;
+    }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    
 </style>
