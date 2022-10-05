7114a80940b4ff12b4d988d102a70f148f5b7ecb
anton <azvontsov@gmail.com>
2022-10-05 10:03:26 -0400

    createWebHashHistory

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import MainPage from "@/pages/MainPage.vue";
 import PostPage from "@/pages/PostPage.vue";
 import ContactPage from "@/pages/ContactPage.vue";
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
